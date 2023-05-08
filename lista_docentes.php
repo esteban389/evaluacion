d14f2f65af537fc7052a7820e9a8955bad80f6a7
Esteban Andres Murcia Acuña <estebandres@proton.me>
2023-05-07 20:37:05 -0500

    testing lista_docentes.php v13

--- lista_docentes.php
+++ lista_docentes.php
@@ -24,7 +24,7 @@ $modulos = get_modulos();
     <a href="lista_docentes.php?close_session=true">cerrar sesión</a>
     <?php
         while($modulos){
-            echo "<a>" . $modulos['Docente']. "</a>";
+            //echo "<a>" . $modulos['Docente']. "</a>";
         }    
     ?>
     
