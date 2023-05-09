c7744e15c4477889429fddd4d7140ca773003d6c
huevo <103962731+esteban389@users.noreply.github.com>
2023-05-09 16:57:50 -0500

    .

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
          FROM Resultados 
          WHERE id_pregunta = 1");
          while($row = mysqli_fetch_array($resultados)){
-            echo $row;
+            echo $row['resultado'];
          }
          ?>
         <input type="submit" class="btn login-btn" name="log-in" value="Iniciar sesión">
