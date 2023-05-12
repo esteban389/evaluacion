5e33844859b4fff75fd56eaf5397cf5a9fbe722b
huevo <103962731+esteban389@users.noreply.github.com>
2023-05-12 15:31:56 -0500

    ..

--- query-functions.php
+++ query-functions.php
@@ -63,9 +63,9 @@
     }
 
     function show_avg(){
-        echo mysqli_fetch_array(mysqli_query($GLOBALS['db'],
+        print_r(mysqli_fetch_array(mysqli_query($GLOBALS['db'],
         "SELECT AVG(resultado) AS promedio
         FROM Resultados
-        WHERE id_pregunta=\"1\" AND id_modulo=\"5\""))['promedio'];
+        WHERE id_pregunta=\"1\" AND id_modulo=\"5\"")));
     }
 ?>
