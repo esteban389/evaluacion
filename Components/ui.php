4bedeb99766809643818bf482e504d2a094ea318
Esteban Andres Murcia Acuña <estebandres@proton.me>
2023-05-13 10:52:39 -0500

    update

--- Components/ui.php
+++ Components/ui.php
@@ -10,16 +10,20 @@ function user_info(){
 
 function mostrar_lista_docentes($row){
     $estado = "";
-    if($row['Estado']==1) $estado="desactivado";
+    $link = $row['id'];
+    if($row['Estado']==1){
+        $estado="desactivado";
+        $link = "#";
+    }
     return "
     <tr class=\"". $estado."\">
                 <td>
-                    <a href=\"evaluacion_docente.php?docente_id=\"". $row['id'] . "\">"
+                    <a href=\"evaluacion_docente.php?docente_id=\"". $link . "\">"
                         . $row['Docente'] .
                     "</a>
                 </td>
                 <td>
-                    <a href=\"evaluacion_docente.php?docente_id=\"". $row['id'] . "\">"
+                    <a href=\"evaluacion_docente.php?docente_id=\"". $link . "\">"
                         . $row['Modulo'] .
                     "</a>
                 </td>
