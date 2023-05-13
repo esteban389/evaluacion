b4266a83b1a59ce0b58e2d6a1853ebfbde11e5f7
huevo <103962731+esteban389@users.noreply.github.com>
2023-05-12 22:16:03 -0500

    ui updates 2

--- Components/ui.php
+++ Components/ui.php
@@ -9,7 +9,7 @@ function user_info(){
 }
 
 function mostrar_lista_docentes($row){
-    return "<a " if($row['id']!=5) echo "class=\"desactivado\"";
+    return "<a "
     ."href=\"evaluacion_docente.php?docente_id=" . $row['id'] . "\">"
     ."<p>"
     . $row['Docente']
